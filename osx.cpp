f7cbd6d3731681a876017b82090a28d385948a2e
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-21 19:55:20 +0000

    implementation based on wagen

--- osx.cpp
+++ osx.cpp
@@ -1,1 +1,22 @@
+module;
+#include <dlfcn.h>
+
 module dl;
+import hai;
+
+namespace dl {
+class osx_lib : public lib {
+  void *m_h;
+
+public:
+  explicit osx_lib(const char *name)
+      : m_h{dlopen(name, RTLD_NOW | RTLD_LOCAL)} {}
+  ~osx_lib() { dlclose(m_h); }
+
+  void *sym(const char *name) { return dlsym(m_h, name); }
+};
+
+hai::uptr<lib> open(const char *name) {
+  return hai::uptr<lib>{new osx_lib(name)};
+}
+} // namespace dl
--- windows.cpp
+++ windows.cpp
@@ -1,1 +1,22 @@
+module;
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 module dl;
+import hai;
+
+namespace dl {
+class win_lib : public lib {
+  HMODULE m_h;
+
+public:
+  explicit win_lib(const char *name) : m_h{LoadLibraryA(name)} {}
+  ~win_lib() { FreeLibrary(m_h); }
+
+  void *sym(const char *name) { return GetProcAddress(m_h, name); }
+};
+
+hai::uptr<lib> open(const char *name) {
+  return hai::uptr<lib>{new win_lib(name)};
+}
+} // namespace dl
