d99f6cc4689f65fd89b99b6f9155979a5528c760
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-22 14:04:08 +0000

    poc: extra print for the modified behaviour

--- poc.cpp
+++ poc.cpp
@@ -11,7 +11,10 @@ int main(int argc, char **argv) {
   silog::log(silog::info, "symbol: %p", fn);
 
   // This second call is here to show a weird bug in MacOS implementation
-  silog::log(silog::info, "mtime: %zd", h->mtime());
+  silog::log(silog::info, "mtime: %zd -- modified: %d", h->mtime(),
+             h->modified());
+  silog::log(silog::info, "mtime: %zd -- modified: %d", h->mtime(),
+             h->modified());
 
   fn();
 }
