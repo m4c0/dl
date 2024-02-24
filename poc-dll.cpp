d464406bc568702b831cc3402c7a6c122b856fe9
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-24 11:13:26 +0000

    fix dll export on windows

--- poc-dll.cpp
+++ poc-dll.cpp
@@ -1,4 +1,10 @@
 #pragma leco dll
 import silog;
 
-extern "C" void hello() { silog::log(silog::info, "inside dll"); }
+#ifdef LECO_TARGET_WINDOWS
+#define A __declspec(dllexport)
+#else
+#define A
+#endif
+
+extern "C" A void hello() { silog::log(silog::info, "inside dll"); }
