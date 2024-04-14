3e6ca39b257ba69c122f5e12f3490ebd8ea29867
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-14 10:50:54 +0100

    poc-dll: proofing the global ctor/dtor is called

--- poc-dll.cpp
+++ poc-dll.cpp
@@ -7,4 +7,9 @@ import silog;
 #define A
 #endif
 
+struct global_scope_test {
+  global_scope_test() { silog::log(silog::info, "dll loaded"); }
+  ~global_scope_test() { silog::log(silog::info, "dll unloaded"); }
+} i;
+
 extern "C" A void hello() { silog::log(silog::info, "inside dll"); }
